f8dfee6e5769f25d41cff35419eaf29b3d778fb7
Poncho Martinez <poncho.811@gmail.com>
2017-05-19 18:42:47 -0500

    Initial commit

---
+++ lib/subwars.ex
@@ -0,0 +1,18 @@
+defmodule Subwars do
+  @moduledoc """
+  Documentation for Subwars.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Subwars.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/subwars_test.exs
@@ -0,0 +1,8 @@
+defmodule SubwarsTest do
+  use ExUnit.Case
+  doctest Subwars
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
